55b1201f636b9b0f98ed8ff844b825ee7e1155a1
Febriansyah-im <79479544+Febriansyah-im@users.noreply.github.com>
2021-06-05 18:10:51 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.toggle').addEventListener("click",function (){
+	document.querySelector(".menu").classList.toggle("active");
+	document.querySelector(".toggle").classList.toggle("active");
+});
