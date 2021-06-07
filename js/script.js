f8509c1b57d8bca2bf986fe7400c90bf02dbaadc
Febriansyah-im <79479544+Febriansyah-im@users.noreply.github.com>
2021-06-07 19:28:07 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-$('.toggle').click(function(){
-	$(".menu").toggleClass("active");
+function Menu(){
 	$(".toggle").toggleClass("active");
-	
-	$(".nav-item").click(function(){
-		$('.nav-item.active').removeClass('active');
-		$(this).addClass('active');
-		$(".toggle.active").removeClass("active");
-		$(".menu.active").removeClass("active");
-	});
+	$(".menu").toggleClass("active");
+};
+
+$(window).click(() =>{
+	if($(event.target).hasClass("toggle"))
+	{
+		Menu();
+	};
+
+	if($(event.target).parent().hasClass("nav-link"))
+	{
+		$(".nav-item.active").removeClass('active');
+		$(event.target).parent().parent().addClass('active');
+		Menu();
+	};
 });
 
+$(window).on('hashchange', ()=>{
+    $("#page-transition").addClass("active");
+	setTimeout(()=>{
+		$("#page-transition").removeClass("active");
+	}, 400);
+
+	const thisPage = location.hash;
+	
+	if($(thisPage).hasClass("hide")){
+	 	$(".section").addClass("hide");
+	 	setTimeout(()=>{
+			$(thisPage).removeClass("hide");
+		}, 430);
+	}
+	
+}).trigger('hashchange');
